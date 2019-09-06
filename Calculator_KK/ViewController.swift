6d073b3476c886f5ac1645c56f98819739670348
starlychen <starly.chen@car-plus.com.tw>
2019-09-06 17:33:05 +0800

    update

--- Calculator_KK/ViewController.swift
+++ Calculator_KK/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var numberOne: UIButton!
+    @IBOutlet weak var numberTwo: UIButton!
+    @IBOutlet weak var numberThree: UIButton!
+    @IBOutlet weak var numberFour: UIButton!
+    @IBOutlet weak var numberFive: UIButton!
+    @IBOutlet weak var numberSix: UIButton!
+    @IBOutlet weak var numberSeven: UIButton!
+    @IBOutlet weak var numberEight: UIButton!
+    @IBOutlet weak var numberNine: UIButton!
+    @IBOutlet weak var numberZero: UIButton!
+    @IBOutlet weak var decimalPoint: UIButton!
+    
+    
+    @IBOutlet weak var addingBtn: UIButton!
+    @IBOutlet weak var subtractingBtn: UIButton!
+    @IBOutlet weak var multiplyingBtn: UIButton!
+    @IBOutlet weak var dividingBtn: UIButton!
+    @IBOutlet weak var outputBtn: UIButton!
+    
+    
+    @IBOutlet weak var memoryCleanBtn: UIButton!
+    @IBOutlet weak var memoryRecallBtn: UIButton!
+    @IBOutlet weak var memoryPlusBtn: UIButton!
+    @IBOutlet weak var memorySubtractBtn: UIButton!
+    
+    var numberArray:[NSInteger] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+
+
     }
+    
+    
+    @IBAction func typeNumberAction(_ sender: UIButton) {
+      
+        self.numberArray.append(sender.tag)
+        print(self.numberArray)
+        
+    }
+    
+    func arrayToInt(inputArray: Array<Any>) -> NSInteger {
+        
+        
+        return 20
+    }
+    
 
 
 }
+
 
