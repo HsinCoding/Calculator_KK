b0e37ffa6618ceee8673f9056bd6fb061aa2348a
starlychen <starly.chen@car-plus.com.tw>
2019-09-06 11:36:29 +0800

    First commit

---
+++ Calculator_KK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator_KK
+//
+//  Created by ＵＳＥＲ on 2019/9/6.
+//  Copyright © 2019 hsin_project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
